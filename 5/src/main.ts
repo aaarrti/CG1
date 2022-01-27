ba9ba8adcf4ad4969990bad989116630fafb6cce
Artem Sereda <Aitem1503@icloud.com>
2022-01-28 00:15:35 +0100

    task 2 done

--- 5/src/main.ts
+++ 5/src/main.ts
@@ -2,7 +2,7 @@
 import { CanvasWidget } from "./canvasWidget";
 import * as helper from "./helper";
 import { Application, createWindow } from "./lib/window";
-import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import { Mesh, MeshPhongMaterial, PerspectiveCamera, Raycaster, Scene, Vector2, WebGLRenderer } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import RenderWidget from "./lib/rendererWidget";
 import type { KeyValuePair } from "./lib/utils";
@@ -10,6 +10,9 @@ import type { KeyValuePair } from "./lib/utils";
 
 let canvasWidget: CanvasWidget;
 let settings: helper.Settings;
+let camera: PerspectiveCamera;
+let scene: Scene;
+let renderer: WebGLRenderer;
 
 
 function callback(changed: KeyValuePair<helper.Settings>) {
@@ -17,12 +20,30 @@ function callback(changed: KeyValuePair<helper.Settings>) {
     case "width":
       canvasWidget.changeDimensions(changed.value, settings.height);
       break;
-    case 'height':
+    case "height":
       canvasWidget.changeDimensions(settings.width, changed.value);
       break;
 
   }
 
+}
+
+
+function raycast() {
+  console.log("Started raycasting");
+  for (var x = 0; x <= settings.width; x += 1) {
+    for (var y = 0; y <= settings.height; y += 1) {
+      const target = new Vector2((2. * x / settings.width) - 1., (2. * y / settings.height) - 1.);
+      const raycaster = new Raycaster();
+      raycaster.setFromCamera(target, camera);
+      let intersections = raycaster.intersectObjects(scene.children, true);
+      if (intersections.length > 0) {
+        canvasWidget.setPixel(x, settings.height - y,
+          ((intersections[0].object as Mesh).material as MeshPhongMaterial).color);
+      }
+    }
+  }
+  console.log("Finished raycasting");
 }
 
 function main() {
@@ -41,24 +62,25 @@ function main() {
   settings = new helper.Settings();
   let gui = helper.createGUI(settings);
   settings.addCallback(callback);
-  settings.saveImg = () => {
-    canvasWidget.savePNG();
-  };
+  settings.saveImg = () => canvasWidget.savePNG();
+  settings.render = raycast;
+
   let rendererDiv = createWindow("renderer");
   root.appendChild(rendererDiv);
-  let renderer = new WebGLRenderer({
+  renderer = new WebGLRenderer({
     antialias: true  // to enable anti-alias and get smoother output
   });
-  let scene = new Scene();
+  scene = new Scene();
   scene = helper.setupGeometry(scene);
   let lights = helper.setupLight(scene);
-  let camera = new PerspectiveCamera();
+  camera = new PerspectiveCamera();
   camera = helper.setupCamera(camera);
   // create controls
   let controls = new OrbitControls(camera, rendererDiv);
   controls = helper.setupControls(controls);
   let wid = new RenderWidget(rendererDiv, renderer, camera, scene, controls);
   wid.animate();
+
 
 }
 
